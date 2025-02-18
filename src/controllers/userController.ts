3ee679d5af3c1dbbc7241a5611ba1b7c096ce899
J0113boy <keskitalo_6@hotmail.com>
2025-02-18 13:23:07 +0100

    Create addUser controller.

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,9 +1,30 @@
 import { Request, Response } from 'express';
+import bcrypt from 'bcryptjs';
 import User from '../models/userModel';
 
 export const addUser = async (req: Request, res: Response) => {
     try {
-        const user = new User(req.body);
+        const { username, email, password } = req.body;
+
+        if (!username || !email || !password) {
+            res.status(400).json({
+                message: 'All fields are required',
+            });
+        }
+
+        const existingUser = await User.findOne({ email: req.body.email });
+        if (existingUser) {
+            return res.status(400).json({
+                message: 'User with this email already exists',
+            });
+        }
+
+        const encryptedPassword = await bcrypt.hash(password, 10);
+        const user = new User({
+            username,
+            email,
+            password: encryptedPassword,
+        });
         await user.save();
         res.status(200).json({
             message: 'User created successfully: ',
