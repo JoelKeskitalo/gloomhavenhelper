10146c044cac7ec82148052356f467e32991127f
J0113boy <keskitalo_6@hotmail.com>
2025-03-14 13:47:16 +0100

    Create updateMonster controller & endpoint.

--- src/controllers/monsterController.ts
+++ src/controllers/monsterController.ts
@@ -33,3 +33,42 @@ export const getMonsterById = async (req: Request, res: Response): Promise<void>
         });
     }
 };
+
+export const createMonster = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const newMonster = new Monster(req.body);
+        const savedMonster = await newMonster.save();
+        res.status(201).json({
+            message: 'Monster created',
+            monster: savedMonster,
+        });
+    } catch (error: unknown) {
+        const err = error as Error;
+        res.status(500).json({
+            error: err.message,
+        });
+    }
+};
+
+export const updateMonster = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { id } = req.params;
+        const updatedMonster = await Monster.findByIdAndUpdate(id, req.body, {
+            new: true,
+            runValidators: true,
+        });
+
+        if (!updatedMonster) {
+            res.status(404).json({
+                message: 'Monster not found',
+            });
+        }
+
+        res.status(200).json(updatedMonster);
+    } catch (error: unknown) {
+        const err = error as Error;
+        res.status(500).json({
+            error: err.message,
+        });
+    }
+};
--- src/routes/monsterRoutes.ts
+++ src/routes/monsterRoutes.ts
@@ -1,10 +1,17 @@
 import express from 'express';
-import { getAllMonsters, getMonsterById } from '../controllers/monsterController';
+import {
+    getAllMonsters,
+    getMonsterById,
+    createMonster,
+    updateMonster,
+} from '../controllers/monsterController';
 
 const router = express.Router();
 
 // /api/monsters
 router.get('/', getAllMonsters);
 router.get('/:id', getMonsterById);
+router.post('/', createMonster);
+router.put('/:id', updateMonster);
 
 export default router;
