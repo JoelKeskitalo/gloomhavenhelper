badbd5d7129ce6360a838697524aba9860131604
J0113boy <keskitalo_6@hotmail.com>
2025-03-14 13:37:28 +0100

    Create getMonsterById controller & endpoint.

--- src/controllers/monsterController.ts
+++ src/controllers/monsterController.ts
@@ -12,3 +12,24 @@ export const getAllMonsters = async (req: Request, res: Response): Promise<void>
         });
     }
 };
+
+export const getMonsterById = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { id } = req.params;
+        const monster = await Monster.findById(id).populate('abilities');
+
+        if (!monster) {
+            res.status(404).json({
+                message: 'Monster not found',
+            });
+            return;
+        }
+
+        res.status(200).json(monster);
+    } catch (error: unknown) {
+        const err = error as Error;
+        res.status(500).json({
+            error: err.message,
+        });
+    }
+};
--- src/routes/monsterRoutes.ts
+++ src/routes/monsterRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getAllMonsters } from '../controllers/monsterController';
+import { getAllMonsters, getMonsterById } from '../controllers/monsterController';
 
 const router = express.Router();
 
 // /api/monsters
 router.get('/', getAllMonsters);
+router.get('/:id', getMonsterById);
 
 export default router;
