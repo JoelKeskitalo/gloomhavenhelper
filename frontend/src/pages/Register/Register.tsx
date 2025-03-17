cf833285552e1b617472170011bfa67ec85d5424
J0113boy <keskitalo_6@hotmail.com>
2025-03-17 07:41:22 +0100

    Create template for Register page.

--- frontend/src/pages/Register/Register.tsx
+++ frontend/src/pages/Register/Register.tsx
@@ -1,7 +1,30 @@
+import { Link } from 'react-router-dom';
+import './Register.scss';
+
 const Register = () => {
     return (
-        <div>
-            <h1>Register</h1>
+        <div className="register-container">
+            <div className="register-box">
+                <h1 className="register-title">Create an Account</h1>
+                <form className="register-form">
+                    <input type="text" placeholder="Username" className="register-input" />
+                    <input type="email" placeholder="Email" className="register-input" />
+                    <input type="password" placeholder="Password" className="register-input" />
+                    <input
+                        type="password"
+                        placeholder="Confirm Password"
+                        className="register-input"
+                    />
+                    <button type="submit" className="register-button">
+                        Register
+                    </button>
+                </form>
+                <div className="register-links">
+                    <Link to="/login" className="login-link">
+                        Already have an account? Login here
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
