cdff519bd5c3d687dbaf3dd1130ceb3a45613b11
J0113boy <keskitalo_6@hotmail.com>
2025-03-17 08:06:57 +0100

    Create Register Page template.

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,7 +1,27 @@
+import { Link } from 'react-router-dom';
+import './Home.scss';
+
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="home-container">
+            <div className="home-box">
+                <h1 className="home-title">Welcome to Gloomhaven Helper</h1>
+                <p className="home-text">Choose a section to begin:</p>
+                <div className="home-links">
+                    <Link to="/hero" className="home-link">
+                        Hero
+                    </Link>
+                    <Link to="/decks" className="home-link">
+                        Decks
+                    </Link>
+                    <Link to="/rules" className="home-link">
+                        Rules
+                    </Link>
+                    <Link to="/account" className="home-link">
+                        Account
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
