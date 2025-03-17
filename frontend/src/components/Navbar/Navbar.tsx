065eae9901d8a6b7b19657e2d34fe6aca92449ca
J0113boy <keskitalo_6@hotmail.com>
2025-03-17 09:13:47 +0100

    Create NavBar template.

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,29 @@
+import { NavLink } from 'react-router-dom';
 import './Navbar.scss';
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <nav className="navbar">
+            <div className="navbar-container">
+                <NavLink to="/" className="navbar-logo">
+                    Gloomhaven Helper
+                </NavLink>
+                <div className="navbar-links">
+                    <NavLink to="/hero" className="navbar-link">
+                        Hero
+                    </NavLink>
+                    <NavLink to="/decks" className="navbar-link">
+                        Decks
+                    </NavLink>
+                    <NavLink to="/rules" className="navbar-link">
+                        Rules
+                    </NavLink>
+                    <NavLink to="/account" className="navbar-link">
+                        Account
+                    </NavLink>
+                </div>
+            </div>
+        </nav>
     );
 };
 
