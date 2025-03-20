0e25338dcdf8962292b237b7dfe532b33f91d33f
J0113boy <keskitalo_6@hotmail.com>
2025-03-20 17:52:54 +0100

    Separate thunks from slices by creating a new folder, thunks, in the redux folder.

--- frontend/src/redux/slices/authSlice.ts
+++ frontend/src/redux/slices/authSlice.ts
@@ -1,58 +1,11 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { AuthState, User, RegisterPayload, LoginPayload } from '../../types/auth';
-import { registerUser, loginUser, getUserById } from '../../api/auth';
+import { createSlice } from '@reduxjs/toolkit';
+import { AuthState } from '../../types/auth';
+import { registerUserThunk, loginUserThunk, fetchUser } from '../thunks/authThunks';
 
 const initialState: AuthState = {
     user: null,
     isAuthenticated: false,
 };
-
-export const registerUserThunk = createAsyncThunk<User, RegisterPayload>(
-    'auth/registerUser',
-    async (credentials, { rejectWithValue }) => {
-        try {
-            const response = await registerUser(credentials);
-            const user = response.user;
-
-            if (user.token) {
-                localStorage.setItem('token', user.token);
-            }
-
-            return user;
-        } catch (error) {
-            return rejectWithValue(error);
-        }
-    }
-);
-
-export const loginUserThunk = createAsyncThunk<User, LoginPayload>(
-    'auth/loginUser',
-    async (credentials, { rejectWithValue }) => {
-        try {
-            const response = await loginUser(credentials);
-            const user = response.user;
-
-            if (user.token) {
-                localStorage.setItem('token', user.token);
-            }
-
-            return user;
-        } catch (error) {
-            return rejectWithValue(error);
-        }
-    }
-);
-
-export const fetchUser = createAsyncThunk<User, string>(
-    'auth/fetchUser',
-    async (userId, { rejectWithValue }) => {
-        try {
-            return await getUserById(userId);
-        } catch (error) {
-            return rejectWithValue(error);
-        }
-    }
-);
 
 const authSlice = createSlice({
     name: 'auth',
---
+++ frontend/src/redux/thunks/authThunks.ts
@@ -0,0 +1,50 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { User, RegisterPayload, LoginPayload } from '../../types/auth';
+import { registerUser, loginUser, getUserById } from '../../api/auth';
+
+export const registerUserThunk = createAsyncThunk<User, RegisterPayload>(
+    'auth/registerUser',
+    async (credentials, { rejectWithValue }) => {
+        try {
+            const response = await registerUser(credentials);
+            const user = response.user;
+
+            if (user.token) {
+                localStorage.setItem('token', user.token);
+            }
+
+            return user;
+        } catch (error) {
+            return rejectWithValue(error);
+        }
+    }
+);
+
+export const loginUserThunk = createAsyncThunk<User, LoginPayload>(
+    'auth/loginUser',
+    async (credentials, { rejectWithValue }) => {
+        try {
+            const response = await loginUser(credentials);
+            const user = response.user;
+
+            if (user.token) {
+                localStorage.setItem('token', user.token);
+            }
+
+            return user;
+        } catch (error) {
+            return rejectWithValue(error);
+        }
+    }
+);
+
+export const fetchUser = createAsyncThunk<User, string>(
+    'auth/fetchUser',
+    async (userId, { rejectWithValue }) => {
+        try {
+            return await getUserById(userId);
+        } catch (error) {
+            return rejectWithValue(error);
+        }
+    }
+);
